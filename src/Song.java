f91764e8bed5d51e5276b84b58e99382a0f4a7b5
Alireza Kafshdartoosi <71192730+AlirezaKToosi@users.noreply.github.com>
2023-05-14 21:00:25 +0200

    Add main class implementation add Song class implementation create MusicPlayer artifact

--- src/Song.java
+++ src/Song.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Song {
+public class Song {
+    private String name;
+    private String artist;
+    private String album;
+    private String genre;
+    private String fileName;
+    private String albumImage;
+
+    public Song(String name, String artist, String album, String genre, String fileName, String albumImage) {
+        this.name = name;
+        this.artist = artist;
+        this.album = album;
+        this.genre = genre;
+        this.fileName = fileName;
+        this.albumImage = albumImage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getAlbumImage() {
+        return albumImage;
+    }
 }
